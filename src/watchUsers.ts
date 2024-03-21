f0e805fd35bb1e3fdfb0753ef0c3b3c453bebd56
Mo9la3 <164370290+Mo9la3@users.noreply.github.com>
2024-03-21 23:28:19 +0000

    Update watchUsers.ts
    last tst

--- src/watchUsers.ts
+++ src/watchUsers.ts
@@ -2,29 +2,26 @@ import recordUser from './recordUser.ts';
 
 export default function watchUsers(usersstr: string, output?: string) {
   const watchUsersIter = () => {
-    const users = usersstr.split(' ');
+    try {
+      const users = usersstr.split(' ');
 
-    const checkUser = (i = 0) => {
-      if (!window.recording[users[i]]) recordUser(users[i], output);
-      else console.log(`\x1b[32mStill recording ${users[i]}\x1b[0m`);
-      setTimeout(() => {
-        if (i < users.length - 1) checkUser(++i);
-        else
-          console.log(
-            `\n\n${new Date().toTimeString().slice(0, 8)}       Waiting for recheck in 3m...\n\n`
-          );
-      }, 3000);
-    };
-    try{
+      const checkUser = (i = 0) => {
+        if (!window.recording[users[i]]) recordUser(users[i], output);
+        else console.log(`Still recording ${users[i]}`);
+        setTimeout(() => {
+          if (i < users.length - 1) checkUser(++i);
+          else
+            console.log(
+              `${new Date().toTimeString().slice(0, 8)}       Waiting for recheck in 3m...\n\n`
+            );
+        }, 3000);
+      };
       checkUser();
+    } catch (error) {
+      console.error('An error occurred but the script will continue:', error);
     }
-    catch (err) {
-      console.error(`====> Error recording user ${users[i]}:`, error);
-      watchUsersIter();
-    }
-    
 
-    setTimeout(watchUsersIter, 70000);
+    setTimeout(watchUsersIter, 180000);
   };
 
   watchUsersIter();
