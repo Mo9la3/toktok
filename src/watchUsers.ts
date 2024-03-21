ca2716c98eb4794cba64a933021b505f2beabc2d
Mo9la3 <164370290+Mo9la3@users.noreply.github.com>
2024-03-21 19:49:43 +0000

    Update watchUsers.ts

--- src/watchUsers.ts
+++ src/watchUsers.ts
@@ -4,22 +4,25 @@ export default function watchUsers(usersstr: string, output?: string) {
   const watchUsersIter = () => {
     const users = usersstr.split(' ');
 
+    const checkUser = (i = 0) => {
+      if (!window.recording[users[i]]) recordUser(users[i], output);
+      else console.log(`\x1b[32mStill recording ${users[i]}\x1b[0m`);
+      setTimeout(() => {
+        if (i < users.length - 1) checkUser(++i);
+        else
+          console.log(
+            `\n\n${new Date().toTimeString().slice(0, 8)}       Waiting for recheck in 3m...\n\n`
+          );
+      }, 3000);
+    };
     try{
-      const checkUser = (i = 0) => {
-        if (!window.recording[users[i]]) recordUser(users[i], output);
-        else console.log(`\x1b[32mStill recording ${users[i]}\x1b[0m`);
-        setTimeout(() => {
-          if (i < users.length - 1) checkUser(++i);
-          else
-            console.log(
-              `\x1b[36m\n\n${new Date().toTimeString().slice(0, 8)}       Waiting for recheck in 3m...\n\n\x1b[0m`
-            );
-        }, 3000);
-      };
-    } catch(err) {
-      console.error(`====> Error recording user ${users[i]}:`, error);
+      checkUser();
     }
-    checkUser();
+    catch (err) {
+      console.error(`====> Error recording user ${users[i]}:`, error);
+      watchUsersIter();
+    }
+    
 
     setTimeout(watchUsersIter, 180000);
   };
