bf0d4fd763939afc0efe11f7f4595ff03d6b20dc
Mo9la3 <164370290+Mo9la3@users.noreply.github.com>
2024-03-21 23:18:27 +0000

    Update recordUser.ts
    color only

--- src/recordUser.ts
+++ src/recordUser.ts
@@ -1,16 +1,14 @@
-
 import { writableStreamFromWriter } from './deps.ts';
 import { path } from './deps.ts';
 
 export default async function recordUser(user: string, output?: string) {
-  console.log(`I'm in recordUser`);
   const roomId = await getRoomId(user);
   if (!roomId) {
     console.error(`\x1b[33mcould not get roomId for ${user}\x1b[0m`);
     return;
   }
   const url = await getLiveUrl(roomId);
-  if (!url || url === undefined) {
+  if (!url) {
     console.error(`\x1b[31mcould not get live url for ${user}\x1b[0m`);
     return;
   }
@@ -39,7 +37,6 @@ export default async function recordUser(user: string, output?: string) {
 }
 
 async function getRoomId(user: string) {
-  console.log(`I'm in getRoomId`);
   return await fetch(`https://www.tiktok.com/@${user}/live`)
     .then(res => res.text())
     .then(text => {
@@ -49,21 +46,18 @@ async function getRoomId(user: string) {
 }
 
 async function isUserInLive(roomId: string): Promise<boolean | undefined> {
-  console.log(`I'm in isUserInLive`);
   return await fetch(`https://www.tiktok.com/api/live/detail/?aid=1988&roomID=${roomId}`)
     .then(res => res.json())
     .then(json => json.LiveRoomInfo?.status != 4);
 }
 
 async function getLiveUrl(roomId: string): Promise<string | undefined> {
-  console.log(`I'm in getLiveUrl`);
   return await fetch(`https://webcast.tiktok.com/webcast/room/info/?aid=1988&room_id=${roomId}`)
     .then(res => res.json())
     .then(json => json.data?.stream_url?.rtmp_pull_url);
 }
 
 function filename(user: string, output?: string): string {
-  console.log(`I'm in filename`);
   const date = new Date();
   const day = date.toISOString().slice(0, 10);
   const time = date.toTimeString().slice(0, 8).replaceAll(':', '-');
