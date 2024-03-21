f193b1f17592303929dfe182ad37a2ce57b8e80e
Mo9la3 <164370290+Mo9la3@users.noreply.github.com>
2024-03-21 20:09:44 +0000

    Update watchUsers.ts

--- src/watchUsers.ts
+++ src/watchUsers.ts
@@ -24,7 +24,7 @@ export default function watchUsers(usersstr: string, output?: string) {
     }
     
 
-    setTimeout(watchUsersIter, 5000);
+    setTimeout(watchUsersIter, 120000);
   };
 
   watchUsersIter();
