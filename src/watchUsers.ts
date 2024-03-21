7a171c950ac43710ae13b6237ec077dddb30267c
Mo9la3 <164370290+Mo9la3@users.noreply.github.com>
2024-03-21 19:21:05 +0000

    Update watchUsers.ts

--- src/watchUsers.ts
+++ src/watchUsers.ts
@@ -11,7 +11,7 @@ export default function watchUsers(usersstr: string, output?: string) {
         if (i < users.length - 1) checkUser(++i);
         else
           console.log(
-            `${new Date().toTimeString().slice(0, 8)}       Waiting for recheck in 3m...\n\n`
+            `\n\n${new Date().toTimeString().slice(0, 8)}       Waiting for recheck in 3m...\n\n`
           );
       }, 3000);
     };
